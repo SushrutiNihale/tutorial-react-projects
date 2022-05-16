dff09f6bf7b1b0e9c5cff3e43b6800739ec750be
Sushruti Nihale <sushrutin6@gmail.com>
2022-05-16 23:41:28 +0530

    working on 2nd project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [LoadingState, setLoadingState] = useState(true);
+  const [ToursData, setToursData] = useState([]);
+
+  const fetchTours = () => {
+    setLoadingState(true); // for redundancy
+
+    try {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          setToursData(data);
+          setLoadingState(false);
+        });
+    } catch (err) {
+      console.log(err);
+      setLoadingState(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (LoadingState) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={ToursData} />
+    </main>
+  )
 }
 
 export default App
